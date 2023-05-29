89f61b9600521e020852b0c716249f47074eb9c6
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-05-29 16:40:20 -0600

    Create Screen.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,14 @@
 import keyPad from '../keyPadArr';
 import calculator from '../styles/Calculator.module.css';
 import genClassName from '../helperFunctions';
+import Screen from './Screen';
 
 const Calculator = () => (
   <div className={calculator.calcContainer}>
-    {keyPad.map((key, i) => <button className={`${calculator[genClassName(i)]} ${calculator.btn}`} type="button" key={key}>{key}</button>)}
+    <Screen />
+    <div className={calculator.keyPad}>
+      {keyPad.map((key, i) => <button className={`${calculator[genClassName(i)]} ${calculator.btn}`} type="button" key={key}>{key}</button>)}
+    </div>
   </div>
 );
 
--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,5 +1,5 @@
 const genClassName = (index) => {
-  if ((index + 1) % 4 === 0) return 'orange';
+  if ((index + 1) % 4 === 0 || index === 18) return 'orange';
   if (index === 16) return 'double';
   return '';
 };
