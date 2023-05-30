9691f40f12740f243a4c623be029f63220f58e9e
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-05-30 10:23:30 -0600

    Condense helpers into helpers.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,5 @@
-import keyPad from '../keyPadArr';
 import calculator from '../styles/Calculator.module.css';
-import { genClassName } from '../helpers';
+import { keyPad, genClassName } from '../helpers';
 import Screen from './Screen';
 
 const Calculator = () => (
