9fcaf20730ca20a9672e73bad1c32c040e7c5f1e
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-05-30 14:39:37 -0600

    Add space to Screen elements

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import screen from '../styles/Screen.module.css';
 
 const Screen = ({ total, next, operation }) => (
-  <div className={screen.screen}>{`${total}${operation}${next}`}</div>
+  <div className={screen.screen}>{`${total} ${operation} ${next}`}</div>
 );
 
 export default Screen;
