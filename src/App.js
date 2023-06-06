6deb343ff7c1168813ae8821f3ad8d262c7aa6e6
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-06-06 14:02:36 -0600

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes, NavLink } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import './App.css';
 import Quote from './components/Quote';
@@ -6,11 +7,16 @@ const App = () => (
   <>
     <header>
       <h1>Math Magicians</h1>
+      <nav>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/calculator">Calculator</NavLink></li>
+      </nav>
     </header>
-    <main>
-      <Quote />
-      <Calculator />
-    </main>
+
+    <Routes>
+      <Route path="/" element={<Quote />} />
+      <Route path="/calculator" element={<Calculator />} />
+    </Routes>
   </>
 );
 
